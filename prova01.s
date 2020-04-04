8581384ec84b9292d6a2489727e73fafd9cec434
vincenzo <s259549@studenti.polito.it>
2020-04-04 18:48:49 +0200

    first commit

---
+++ prova01.s
@@ -0,0 +1,19 @@
+			#scrittura di un valore in una cella
+			#di memoria
+	
+	
+			.data
+variabile:	.space 4 #int variabile
+ 
+			.text
+			.globl main
+			.ent main
+
+main:
+			li $t0,19591501 #variabile = 19591501 (012A F14D hex)
+			sw $t0, variabile
+			
+			li $v0,10
+			syscall
+			
+			.end main
