5e8904de256944cc4e730afe56d23318220fc374
vins <vincenzo.mezzela@gmail.com>
2020-05-25 19:06:20 +0200

    update

--- Lab05/es04.s
+++ Lab05/es04.s
@@ -6,4 +6,12 @@ mat:        .word 1,4,5,6,7,4,2,8,6,4,5,8,3,2,9,6,6,2,4,4,7,4,9,4,5
             .globl main
             .ent main
 main:
+            lw $t1,mat
+            li $t0,0 #contatore righe
             
+loop:       bne $t1,$0,error
+
+
+
+
+      
